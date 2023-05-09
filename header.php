f543efddae47edb8180569931f80dd0d10b4181e
Ebenezer Okorie <ebene4christ@gmail.com>
2023-05-09 14:23:01 +0100

    theceo_theme

---
+++ header.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Header template.
+ * 
+ * @package the_ceo
+ */
+ ?> 
+  <!doctype html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>WordPres theme</title>
+    </head>
+    <body>
+    <header>Header</header>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Main template file.
+ * 
+ * @package the_ceo
+ */
+
+ get_header();
+ ?>
+
+    <div class="content">
+        Content
+    </div>
+
+    <?php 
+
+    get_footer();
